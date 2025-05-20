a6bb4d2568b35fdaab8144300203f899d8f4f789
Pedro Mariutti <pmariutti@gmail.com>
2025-05-20 17:51:51 -0300

    correcao menu de hamburguer, form

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const hamburger = document.querySelector(".hamburger")
-const nav = document.querySelector(".nav");
+const nav = document.querySelector(".navbar");
+const navLinks = document.querySelectorAll(".nav-item a");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"))
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        if (nav.classList.contains("active")) { 
+            nav.classList.remove("active"); 
+        }
+    });
+});
+
